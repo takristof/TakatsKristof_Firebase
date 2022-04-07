f0217845b01b367e4f722662958b31b87cfa0761
Kristof <takats.kristof.laszlo@gmail.com>
2022-04-07 18:05:52 +0200

    Connected to firebase and creating asked activities

---
+++ app/src/main/java/com/example/takatskristof_restapi/ListResultActivity.java
@@ -0,0 +1,14 @@
+package com.example.takatskristof_restapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_result);
+    }
+}
